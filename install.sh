3246c9ec8f4732e22b95df8fdd4003bc08087c3e
JP Bochi <jpbochi@protonmail.ch>
2018-07-03 15:18:49 +0200

    exits early if asked to not overwrite and file exists

--- install.sh
+++ install.sh
@@ -26,17 +26,21 @@ while getopts ':v:D:fnx' OPT; do
 done
 shift $((OPTIND-1))
 
-if [ -z "${DRUN_VERSION:-}" ]; then
-  echo 'resolving version to install…'
-  DRUN_VERSION=$(curl -sS https://api.github.com/repos/jpbochi/drun/releases/latest | grep '"tag_name"' | sed 's/.*"tag_name": "v\(.*\)".*/\1/')
-  # DRUN_VERSION=$(curl -sSfI https://github.com/jpbochi/drun/releases/latest | grep '^Location:' | sed 's/^.*\/v//')
-fi
-
 INSTALL_DIR=${INSTALL_DIR:-'/usr/local/bin'}
 INSTALL_PATH="${INSTALL_DIR}/drun"
 INSTALL_DIR=$(dirname "$INSTALL_PATH")
 
-echo "installing drun v${DRUN_VERSION} to ${INSTALL_PATH}…"
+if [ "${MV_OPTS}" = '-n' ] && [ -e "$INSTALL_PATH" ]; then
+  echo >&2 "Not overwriting $INSTALL_PATH"
+  exit 0;
+fi
+
+if [ -z "${DRUN_VERSION:-}" ]; then
+  echo 'Determining latest version…'
+  DRUN_VERSION=$(curl -sS https://api.github.com/repos/jpbochi/drun/releases/latest | grep '"tag_name"' | sed 's/.*"tag_name": "v\(.*\)".*/\1/')
+fi
+
+echo "Installing drun v${DRUN_VERSION} to ${INSTALL_PATH}…"
 TEMP_DIR=$(mktemp -d)
 mkdir -p "$INSTALL_DIR"
 
